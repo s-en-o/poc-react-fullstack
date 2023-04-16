6ac13d862554eb40104f6ae816fcb559aa34f5b8
Seno <seno.is.dev@gmail.com>
2023-04-16 22:31:51 +1200

    feat: fetch data from an API using Axios

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,9 +1,29 @@
 import { useParams } from 'react-router-dom';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Article = () => {
     const { articleId } = useParams();
+
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+
+            const newArticleInfo = response.data;
+
+            setArticleInfo(newArticleInfo);
+        };
+
+        loadArticleInfo();
+    }, [articleId]);
+
     const article = articles.find((article) => article.name === articleId);
 
     if (!article) return <NotFoundPage />;
@@ -11,7 +31,7 @@ const Article = () => {
     return (
         <>
             <h1>{article.title}</h1>
-
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
             {article.content.map((paragraph, index) => (
                 <p key={index}>{paragraph}</p>
             ))}
